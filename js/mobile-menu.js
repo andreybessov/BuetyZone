9af03de94113d129c6c2e7d486333dbe175cdb15
andreybessov <148684816+andreybessov@users.noreply.github.com>
2024-02-08 17:32:18 +0200

    Add files via upload

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,16 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
-const menuBtnClose1 = document.getElementById("mobile-menu-1");
-const menuBtnClose2 = document.getElementById("mobile-menu-2");
-const menuBtnClose3 = document.getElementById("mobile-menu-3");
-const menuBtnClose4 = document.getElementById("mobile-menu-4");
+const menuNavLink = document.getElementById("mobile-link-1");
+const menuNavLink2 = document.getElementById("mobile-link-2");
+const menuNavLink3 = document.getElementById("mobile-link-3");
+const menuNavLink4 = document.getElementById("mobile-link-4");
 
 const toggleMenu = () => mobileMenu.classList.toggle("is-open");
 
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
-menuBtnClose1.addEventListener("click", toggleMenu);
-menuBtnClose2.addEventListener("click", toggleMenu);
-menuBtnClose3.addEventListener("click", toggleMenu);
-menuBtnClose4.addEventListener("click", toggleMenu);
+menuNavLink.addEventListener("click", toggleMenu);
+menuNavLink2.addEventListener("click", toggleMenu);
+menuNavLink3.addEventListener("click", toggleMenu);
+menuNavLink4.addEventListener("click", toggleMenu);
